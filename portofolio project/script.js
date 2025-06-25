7b83586edd72a4cd21cc8bab073b183bb5021e75
sutan1817 <sutan.sinaga.18@gmail.com>
2025-06-25 16:35:20 +0700

    update portofolio project/script.js

--- portofolio project/script.js
+++ portofolio project/script.js
@@ -1,4 +1,4 @@
-const dropbutton =document.getElementById("dropbutton");
-dropbutton.addEventListener("click",event=>{event.target.style.transform:rotateX(180deg).toggle;);
+const dropbutton =document.getElementByClassName("dropbutton");
+dropbutton
 
 
