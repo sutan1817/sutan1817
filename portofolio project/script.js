a834c10d44a825082832d7f596f1e1c2adfd8ccc
sutan1817 <sutan.sinaga.18@gmail.com>
2025-06-28 16:13:10 +0700

    update portofolio project/script.js

--- portofolio project/script.js
+++ portofolio project/script.js
@@ -2,6 +2,6 @@ const hamburger = document.getElementById('dropbutton');
 const dropdownMenu = document.getElementById('dropdownMenu');
 
 dropbutton.addEventListener('click', () => {
-  hamburger.classList.toggle('rotate');
+  dropbutton.classList.toggle('rotate');
   dropdownMenu.classList.toggle('active');
 });
